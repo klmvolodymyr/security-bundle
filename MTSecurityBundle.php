96c5632581da3661d633bbf39ed98b195bcda90c
Volodymyr Klymniuk <Volodymyr.Klymniuk@gmail.com>
2023-01-27 23:08:03 +0200

    markup for next module

---
+++ MTSecurityBundle.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace SecurityBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * DTSecurityBundle
+ */
+class DTSecurityBundle extends Bundle
+{
+}
